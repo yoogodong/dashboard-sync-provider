b7bf4161637725f8c86dda5121c27c3506c5db4e
yoogodong <yoogodong@gmail.com>
2022-11-07 14:20:46 +0800

    enable jira url and pageSize configurable

--- src/main/java/dong/yoogo/infrastructure/JiraClientImpl.java
+++ src/main/java/dong/yoogo/infrastructure/JiraClientImpl.java
@@ -4,8 +4,9 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.node.ArrayNode;
 import dong.yoogo.application.JiraClient;
 import dong.yoogo.application.jira.ResultIN;
-import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -16,16 +17,20 @@ import java.util.function.Consumer;
 /**
  *
  */
-@AllArgsConstructor
+
 @Slf4j
 @Service
+@PropertySource("classpath:config/dashboard-sync-provider.properties")
 public class JiraClientImpl implements JiraClient {
-    private RestTemplate jiraRest;
-    private final String url = "https://jira.atlassian.com/rest/api/latest/search?jql={jql}&fields={fields}&expand=changelog,names&startAt={startAt}&maxResults={maxResults}";
-    private final String fields = "id,project,issuetype,status,updated,created";
-    private final int pageSize = 10;
-    private final String projectUrl = "https://jira.atlassian.com/rest/api/latest/project";
+    private final RestTemplate jiraRest;
+    @Value("${provider.jira.hostAndContext}")
+    private String hostAndContext;
+    @Value("${provider.jira.pageSize}")
+    private int pageSize;
 
+    public JiraClientImpl(RestTemplate jiraRest) {
+        this.jiraRest = jiraRest;
+    }
 
     /**
      * query issues  from jira
@@ -40,7 +45,9 @@ public class JiraClientImpl implements JiraClient {
     }
 
     private void pagingQuery(String pid, String updatedFrom, int startAt, Consumer<ResultIN> resultINConsumer) {
+        String url = hostAndContext+"/latest/search?jql={jql}&fields={fields}&expand=changelog,names&startAt={startAt}&maxResults={maxResults}";
         String jql = "updated>='" + updatedFrom + "'  AND  project = " + pid;
+        String fields = "id,project,issuetype,status,updated,created";
         final ResultIN resultIN = jiraRest.getForObject(url, ResultIN.class, jql, fields, startAt, pageSize);
         if (resultIN == null) {
             log.error("不能从 jira 获取 issue 数据");
@@ -60,7 +67,8 @@ public class JiraClientImpl implements JiraClient {
      */
     @Override
     public List<String> getAllProjectsId() {
-        final ArrayNode forObject = jiraRest.getForObject(projectUrl, ArrayNode.class);
+        String url = hostAndContext+"/latest/project";
+        final ArrayNode forObject = jiraRest.getForObject(url, ArrayNode.class);
         final List<String> ids = new ArrayList<>();
         if (forObject == null) {
             log.error("不能从 jira 获取 issue 数据");
@@ -71,6 +79,4 @@ public class JiraClientImpl implements JiraClient {
         }
         return ids;
     }
-
-
 }
