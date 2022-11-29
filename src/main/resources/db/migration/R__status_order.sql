8bd548ca2d2af24eebf9a5b6cbfc098b47ae9254
yoogodong <yoogodong@gmail.com>
2022-11-29 14:49:56 +0800

    状态顺序表的内容调整

--- src/main/resources/db/migration/R__status_order.sql
+++ src/main/resources/db/migration/R__status_order.sql
@@ -1,4 +1,14 @@
--- 需求任务的价值流
+-- issue 的状态顺序
+drop table if exists status_order;
+create table status_order
+(
+    issue_type_id integer,
+    status_id     integer,
+    order_id      integer,
+    primary key (issue_type_id, status_id)
+);
+
+-- 状态顺序
 insert into status_order(issue_type_id, status_id, order_id)
 VALUES (12400, 10416, 1);
 insert into status_order(issue_type_id, status_id, order_id)
@@ -8,14 +18,18 @@ VALUES (12400, 11703, 3);
 insert into status_order(issue_type_id, status_id, order_id)
 VALUES (12400, 11705, 4);
 insert into status_order(issue_type_id, status_id, order_id)
-VALUES (12400, 10414, 5);
+VALUES (12400, 11706, 5);
 insert into status_order(issue_type_id, status_id, order_id)
-VALUES (12400, 10101, 6);
+VALUES (12400, 10414, 6);
 insert into status_order(issue_type_id, status_id, order_id)
-VALUES (12400, 10409, 7);
+VALUES (12400, 10101, 7);
 insert into status_order(issue_type_id, status_id, order_id)
-VALUES (12400, 10219, 8);
+VALUES (12400, 10100, 8);
 insert into status_order(issue_type_id, status_id, order_id)
-VALUES (12400, 10213, 9);
+VALUES (12400, 10219, 9);
 insert into status_order(issue_type_id, status_id, order_id)
-VALUES (12400, 10214, 10);
+VALUES (12400, 3, 10);
+insert into status_order(issue_type_id, status_id, order_id)
+VALUES (12400, 10213, 11);
+insert into status_order(issue_type_id, status_id, order_id)
+VALUES (12400, 10214, 12);
--- src/main/resources/db/migration/V1.3.0__create_status_order_table.sql
+++
@@ -1,8 +0,0 @@
--- issue 的状态顺序
-create table status_order
-(
-    issue_type_id integer,
-    status_id     integer,
-    order_id      integer,
-    primary key (issue_type_id, status_id)
-);
