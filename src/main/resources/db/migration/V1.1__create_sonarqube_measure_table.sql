4d8c5e05f016f24f8d41030ee54ee0fc862181fa
yoogodong <yoogodong@gmail.com>
2022-12-20 11:12:31 +0800

    取消格式化，否则 flyway 验证通不过

--- src/main/resources/db/migration/V1.1__create_sonarqube_measure_table.sql
+++ src/main/resources/db/migration/V1.1__create_sonarqube_measure_table.sql
@@ -1,10 +1,9 @@
 -- sonar measure history
-create table measure
-(
-    id      bigint primary key auto_increment,
-    metric  varchar(30),
-    updated datetime(6),
-    value   varchar(30)
+create table measure(
+  id    bigint primary key auto_increment,
+  metric    varchar(30),
+  updated   datetime(6),
+  value     varchar(30)
 );
 
 
--- src/main/resources/db/migration/V1.2__append_project_column_to_measure_table.sql
+++ src/main/resources/db/migration/V1.2__append_project_column_to_measure_table.sql
@@ -1,6 +1,5 @@
 -- sonar measure history
-alter table measure
-    add project varchar(30);
+alter table measure add project varchar(30);
 
 
 
